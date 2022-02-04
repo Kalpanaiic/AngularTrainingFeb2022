7557f63ec3cd8e1840e5b5b66280fff85c329afc
Kalpana D <kalpana.d@iinterchange.net>
2022-02-04 10:29:37 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'priceCard';
+  data= [
+    { 
+      Type: "FREE",
+      Price: 0,
+      User: "Single User",
+      Storage: "5GB Storage",
+      PublicAcess: "Unlimited Public Projects",
+      Community: "Community Access",
+      PrivateAccess: "Unlimited Private Projects",
+      Support: "Dedicated Phone Support",
+      Domain: "Free Subdomain",
+      Report: "Monthly Status Reports"
+    },
+    {
+      Type: "PLUS",
+      Price: 9,
+      User: "5 Users",
+      Storage: "50GB Storage",
+      PublicAcess: "Unlimited Public Projects",
+      Community: "Community Access",
+      PrivateAccess: "Unlimited Private Projects",
+      Support: "Dedicated Phone Support",
+      Domain: "Free Subdomain",
+      Report: "Monthly Status Reports"
+    },
+    {
+      Type:"PRO",
+      Price: 49,
+      User: "Unlimited Users",
+      Storage: "150GB Storage",
+      PublicAcess: "Unlimited Public Projects",
+      Community: "Community Access",
+      PrivateAccess: "Unlimited Private Projects",
+      Support: "Dedicated Phone Support",
+      Domain: "Unlimited Free Subdomain",
+      Report: "Monthly Status Reports"
+    }
+  ];
 }
