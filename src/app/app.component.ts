b8247e81235035c6d39a8bb00cdf1e467fa12a98
Kalpana D <kalpana.d@iinterchange.net>
2022-02-09 16:16:06 +0530

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+interface PriceCard {
+  Type: string,
+  Price: Number,
+  User: String,
+  Storage: String,
+  PublicAccess: String,
+  Community: String,
+  PrivateAccess: String,
+  Support: String,
+  Domain: String,
+  Report: String
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,13 +19,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'priceCard';
-  data= [
+  priceCard: Array<PriceCard> = [
     { 
       Type: "FREE",
       Price: 0,
       User: "Single User",
       Storage: "5GB Storage",
-      PublicAcess: "Unlimited Public Projects",
+      PublicAccess: "Unlimited Public Projects",
       Community: "Community Access",
       PrivateAccess: "Unlimited Private Projects",
       Support: "Dedicated Phone Support",
@@ -25,7 +37,7 @@ export class AppComponent {
       Price: 9,
       User: "5 Users",
       Storage: "50GB Storage",
-      PublicAcess: "Unlimited Public Projects",
+      PublicAccess: "Unlimited Public Projects",
       Community: "Community Access",
       PrivateAccess: "Unlimited Private Projects",
       Support: "Dedicated Phone Support",
@@ -37,12 +49,12 @@ export class AppComponent {
       Price: 49,
       User: "Unlimited Users",
       Storage: "150GB Storage",
-      PublicAcess: "Unlimited Public Projects",
+      PublicAccess: "Unlimited Public Projects",
       Community: "Community Access",
       PrivateAccess: "Unlimited Private Projects",
       Support: "Dedicated Phone Support",
       Domain: "Unlimited Free Subdomain",
       Report: "Monthly Status Reports"
     }
-  ];
+  ]
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+interface PriceCard {
+  Type: string,
+  Price: Number,
+  User: String,
+  Storage: String,
+  PublicAccess: String,
+  Community: String,
+  PrivateAccess: String,
+  Support: String,
+  Domain: String,
+  Report: String
+}
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  @Input() priceCardData: PriceCard = {
+    Type: '',
+  Price: 0,
+  User: '',
+  Storage: '',
+  PublicAccess: '',
+  Community: '',
+  PrivateAccess: '',
+  Support: '',
+  Domain: '',
+  Report: ''
+  }
 
   constructor() { }
 
